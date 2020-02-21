5a42f4f909f680ec53fc2285104da618ddfbc140
byalexeykh <alek8901@gmail.com>
2020-02-21 22:50:40 +0100

    first commit | circle gen

---
+++ Source/Liquid/Private/ProcLiquidMesh.cpp
@@ -0,0 +1,179 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProcLiquidMesh.h"
+#include "..\Public\ProcLiquidMesh.h"
+
+// Sets default values
+AProcLiquidMesh::AProcLiquidMesh()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+
+	mesh = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("ProceduralMesh"));
+	mesh->SetupAttachment(RootComponent);
+}
+
+void AProcLiquidMesh::PostLoad()
+{
+	Super::PostLoad();
+	GenerateMesh();
+}
+
+void AProcLiquidMesh::PostActorCreated()
+{
+	Super::PostActorCreated();
+	GenerateMesh();
+}
+
+
+
+
+
+void AProcLiquidMesh::AddTriangle(int32 V1, int32 V2, int32 V3)
+{
+	Triangles.Add(V1);
+	Triangles.Add(V2);
+	Triangles.Add(V3);
+}
+
+void AProcLiquidMesh::AddUV(float U, float V)
+{
+	UVs.Add(FVector2D(U, V));
+}
+
+void AProcLiquidMesh::AddVertex(int32& Index, int32 Y, int32 Z) // only 2 coords cuz we are building a circle in 2d space
+{
+	Verticies.Add(FVector(0, Y, Z));
+	Index++;
+}
+
+void AProcLiquidMesh::AddVertex(int32 Y, int32 Z) // only 2 coords cuz we are building a circle in 2d space
+{
+	Verticies.Add(FVector(0, Y, Z));
+}
+
+void AProcLiquidMesh::AddVertexColor(FColor Color)
+{
+	Colors.Add(Color);
+}
+
+void AProcLiquidMesh::AddNormal(FVector normal)
+{
+	Normals.Add(normal);
+}
+
+void AProcLiquidMesh::AddTangent(FProcMeshTangent tangent)
+{
+	Tangents.Add(tangent);
+}
+
+void AProcLiquidMesh::AddCircle(FVector Center, int32 Radius, int32 Resolution)
+{
+	float angleBetweenPointsRads = (360 / Resolution) * PI / 180;
+	UE_LOG(LogTemp, Warning, TEXT("Angle between point in rads is = %f"), angleBetweenPointsRads);
+	float currentAngleRads = 0;
+	FVector2D UVCenter = FVector2D(0, 0);
+	vertexIndex = 0; // resetting VertexIndex
+	Verticies.Add(Center); // Adding Center first (Verticies[vertexIndex] = Verticies[0] = Center)
+	UVs.Add(UVCenter);
+
+	for (int i = 0; i <= Resolution; i++) 
+	{
+		float Y = Radius * cos(currentAngleRads);
+		float Z = Radius * sin(currentAngleRads);
+		AddVertex(Y, Z);
+		UE_LOG(LogTemp, Warning, TEXT("Added vertex of num %f with coords: Y: %f, Z: %f"), i, Y, Z);
+		UE_LOG(LogTemp, Warning, TEXT("using currentAngleRads = %f"), currentAngleRads);
+		vertexIndex++;
+
+		float U = 1 * cos(currentAngleRads);
+		float V = 1 * sin(currentAngleRads);
+		AddUV(U, V);
+
+		FVector ThisNormal = FVector::CrossProduct(GetVertexByID(0), GetVertexByID(i)).GetSafeNormal();
+		AddNormal(ThisNormal);
+		AddTangent(FProcMeshTangent(0, 1, 0));
+		AddVertexColor(FColor::Green);
+
+		currentAngleRads += angleBetweenPointsRads;
+	}
+
+	for (int i = 1; i <= Resolution; i++) 
+	{
+		AddTriangle(0, i + 1, i); //рассчитать vertexIndex для алгоритма добавления треугольников
+	}
+
+}
+
+FVector AProcLiquidMesh::GetVertexByID(int32 ID)
+{
+	int32 c = vertexIndex;
+	if (ID <= c)
+		return Verticies[ID];
+	else {
+		UE_LOG(LogTemp, Error, TEXT("ERROR: provided VertexID is out of bounds of Verticies array! Returninig Zero-Vector... (Verticies.Num: %i | Provided ID: %i)"), c, ID);
+		return FVector(0, 0, 0);
+	}
+}
+
+void AProcLiquidMesh::GenerateMesh()
+{
+	Verticies.Reset();
+	Normals.Reset();
+	Tangents.Reset();
+	UVs.Reset();
+	Triangles.Reset();
+	Colors.Reset();
+
+	//vertexIndex = 0;
+	//
+	//AddVertex(0, 10);
+	//AddVertex(vertexIndex, 10, 0);
+	//AddVertex(vertexIndex, 0, 0);
+	//
+	//
+	//AddTriangle(vertexIndex - 2, vertexIndex - 1, vertexIndex);
+
+	AddCircle(MeshCenter, 100, 30);
+
+	UE_LOG(LogTemp, Warning, TEXT("vertexIndex (max) = %i"), vertexIndex);
+	
+	//for (int i = 0; i < 3; i++) 
+	//{
+	//	FVector ThisNormal = FVector::CrossProduct(GetVertexByID(0), GetVertexByID(1)).GetSafeNormal();
+	//	AddNormal(ThisNormal);
+	//	AddTangent(FProcMeshTangent(0, 1, 0));
+	//	AddVertexColor(FColor::Green);
+	//}
+	//
+	//AddUV(0, 1);
+	//AddUV(1, 0);
+	//AddUV(0, 0);
+
+	mesh->CreateMeshSection(0, Verticies, Triangles, Normals, UVs, Colors, Tangents, false); 
+	mesh->bUseAsyncCooking = true;
+}
+
+
+
+
+
+// Called when the game starts or when spawned
+void AProcLiquidMesh::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProcLiquidMesh::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
---
+++ Source/Liquid/Public/ProcLiquidMesh.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "ProcLiquidMesh.generated.h"
+
+UCLASS()
+class LIQUID_API AProcLiquidMesh : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	TArray<FVector> Normals;
+	TArray<FProcMeshTangent> Tangents;
+	TArray<FVector2D> UVs;
+	TArray<int32> Triangles;
+	TArray<FColor> Colors;
+
+	int32 TrisIndex;
+	int32 vertexIndex;
+	FVector MeshCenter = FVector(0, 0, 0);
+
+	UProceduralMeshComponent* mesh;
+
+	USceneComponent* Root;
+
+	void PostLoad() override;
+	void PostActorCreated() override;
+
+	void AddTriangle(int32 V1, int32 V2, int32 V3);
+	void AddUV(float V1, float V2);
+	void AddVertex(int32& Index, int32 V2, int32 V3);
+	void AddVertex(int32 V2, int32 V3);
+	void AddVertexColor(FColor Color);
+	void AddNormal(FVector normal);
+	void AddTangent(FProcMeshTangent tangent);
+	void AddCircle(FVector Center, int32 Radius, int32 Resolution);
+	FVector GetVertexByID(int32 ID);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void GenerateMesh();
+	TArray<FVector> Verticies;
+
+	// Sets default values for this actor's properties
+	AProcLiquidMesh();
+};
